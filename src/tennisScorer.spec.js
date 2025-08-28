b7fa4cd733dfd59eb9e9c0d261d4a1b5e6820755
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-28 10:19:22 -0400

    agregando una las primera tres pruebas de tenis que seria los dos en cero, el jugador uno a cer, el jugador 1 dos a cero

---
+++ src/tennisScorer.spec.js
@@ -0,0 +1,21 @@
+import { TennisScorer } from "./tennisScorer.js";
+
+describe("TennisScorer", () => {
+    it("game empieza con ambos en cero", () => {
+        const score = new TennisScorer();
+        expect(score.showScore()).toEqual("Love - Love");
+    });
+
+    it("player 1 anota un punto y el jugador no anota nada 1-0", () => {
+        const score = new TennisScorer();
+        score.player1Scores();
+        expect(score.showScore()).toEqual("15 - Love");
+    });
+
+    it("player 1 anota dos veces y el juegador no anota nada 2-0", () => {
+        const score = new TennisScorer();
+        score.player1Scores();
+        score.player1Scores();
+        expect(score.showScore()).toEqual("30 - Love");
+    });
+});
